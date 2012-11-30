40d85a5f9e7fa3f47e64a80a118bab0fc8255d89
Rafal Lukaszewski <solarplexus6@gmail.com>
2012-11-30 21:02:04 +0100

    gof interface initial

--- elm/main.elm
+++ elm/main.elm
@@ -1,17 +1,38 @@
-import Signal.Window (dimensions)
+import Window (dimensions)
+import Mouse (clicks,position)
+
+repeat a n = map (\_ -> a) [1..n]
+
+clickLocations = foldp (:) [] (sampleOn clicks position)
 
 placeholder w h col text = color col $ container w h middle $ plainText text
+
+data GameOfLife = GameOfLife [(Int, Int)]
+type State = GameOfLife
+
+gofAutomaton =
+  let fstep pos fs =
+          let fs' = fs ++ [filled cyan (ngon 5 20 pos)]
+          in  unzip $ map (\f -> step f mouse) fs'
+  in  init' [filled cyan (ngon 5 20 (200,200)) ] fstep
 
 synthCtrl w h = placeholder w h blue "Sythesizer controls"
 golCtrl w h = placeholder w h (rgb 56 56 56) "GoL controls"
 presets w h = placeholder w h (rgb 56 56 56) "Presets"
-sequencer w h = placeholder w h black "Sequencer"
+sequencer w h = let { steps = 16.0
+                    ; cellSize = toFloat(w)/steps
+                    ; coordinates = map (\x -> cellSize*toFloat(x) - cellSize*0.5) [1..steps]
+                    ; grid = concatMap (\c -> zip (repeat c steps) coordinates) coordinates }
+                    ; inactiveCells = map (filled (rgb 64 64 64) . rect (cellSize*0.8) (cellSize*0.8)) $ grid
+                in
+                    lift (\activeCelss -> collage w h $ inactiveCells ++ active)
+                    $ run gofAutomaton $ sampleOn clicks position
 
 percent x p = (x * p) `div` 100
 
 view (w,h) = 
     let layout_w = (w * 5) `div` 6 in
-    let game_h = (h * 3) `div` 5 in    
+    let game_h = (h * 3) `div` 5 in
     container w h middle $ color grey $
         (container layout_w game_h middle $ flow right 
             [
