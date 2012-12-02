1dc25561e430ee7983050d0b828c828726a46bd0
Rafal Lukaszewski <solarplexus6@gmail.com>
2012-12-02 23:47:17 +0100

    click automaton

--- elm/main.elm
+++ elm/main.elm
@@ -1,5 +1,6 @@
 import Window (dimensions)
 import Mouse (clicks,position)
+import Automaton
 
 repeat a n = map (\_ -> a) [1..n]
 
@@ -11,10 +12,10 @@ data GameOfLife = GameOfLife [(Int, Int)]
 type State = GameOfLife
 
 gofAutomaton =
-  let fstep pos fs =
-          let fs' = fs ++ [filled cyan (ngon 5 20 pos)]
-          in  unzip $ map (\f -> step f mouse) fs'
-  in  init' [filled cyan (ngon 5 20 (200,200)) ] fstep
+    let fstep (x,y) fs = 
+        let out = (filled (rgb 255 102 0) (rect 10 10 (x/5, y/5))):fs 
+        in (out,out)
+    in init' [filled (rgb 255 102 0) (rect 10 10 (0,0)) ] fstep
 
 synthCtrl w h = placeholder w h blue "Sythesizer controls"
 golCtrl w h = placeholder w h (rgb 56 56 56) "GoL controls"
@@ -22,25 +23,28 @@ presets w h = placeholder w h (rgb 56 56 56) "Presets"
 sequencer w h = let { steps = 16.0
                     ; cellSize = toFloat(w)/steps
                     ; coordinates = map (\x -> cellSize*toFloat(x) - cellSize*0.5) [1..steps]
-                    ; grid = concatMap (\c -> zip (repeat c steps) coordinates) coordinates }
-                    ; inactiveCells = map (filled (rgb 64 64 64) . rect (cellSize*0.8) (cellSize*0.8)) $ grid
+                    ; grid = concatMap (\c -> zip (repeat c steps) coordinates) coordinates
+                    ; inactiveCells = map (filled (rgb 64 64 64) . rect (cellSize*0.8) (cellSize*0.8)) $ grid }
                 in
-                    lift (\activeCelss -> collage w h $ inactiveCells ++ active)
+                    lift (\activeCells -> collage w h $ inactiveCells ++ activeCells) 
                     $ run gofAutomaton $ sampleOn clicks position
 
 percent x p = (x * p) `div` 100
 
-view (w,h) = 
-    let layout_w = (w * 5) `div` 6 in
-    let game_h = (h * 3) `div` 5 in
-    container w h middle $ color grey $
-        (container layout_w game_h middle $ flow right 
-            [
-                golCtrl (percent layout_w 12) (percent game_h 80),
-                sequencer (percent game_h 90) (percent game_h 90),
-                presets (percent layout_w 12) (percent game_h 80)
-            ])
-        `above`
-        synthCtrl layout_w (h `div` 6)
+view (w,h) seqView = 
+    let {
+        layout_w = (w * 5) `div` 6
+    ;   game_h = (h * 3) `div` 5
+    --;   seqView = sequencer (percent game_h 90) (percent game_h 90)
+    }
+    in container w h middle $ color grey $
+            (container layout_w game_h middle $ flow right 
+                [
+                    golCtrl (percent layout_w 12) (percent game_h 80),
+                    seqView,
+                    presets (percent layout_w 12) (percent game_h 80)
+                ])
+            `above`
+            synthCtrl layout_w (h `div` 6) 
 
-main = lift view dimensions
+main = lift2 view dimensions $ sequencer 400 400
