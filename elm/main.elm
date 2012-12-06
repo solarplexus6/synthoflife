f1a576511734db81e27c5e9f58cb864468a6c742
Rafal Lukaszewski <solarplexus6@gmail.com>
2012-12-06 02:17:10 +0100

    gof almost ready

--- elm/main.elm
+++ elm/main.elm
@@ -2,16 +2,29 @@ import Window (dimensions)
 import Mouse (clicks,position)
 import Time (every)
 import Automaton
-import Set(insert, singleton, toList)
+import Dict as Dict
 
 repeat a n = map (\_ -> a) [1..n]
 
 clickLocations = foldp (:) [] (sampleOn clicks position)
 
 placeholder w h col text = color col $ container w h middle $ plainText text
+dictSize d = Dict.foldl (\_ _ y -> y+1) 0 d
 
-type GameOfLife = Set (Int, Int)
+--type GameOfLife = Dict String (Int, Int) -- Dict nie obsluguje pary jako klucz
 --type State = GameOfLife
+
+adjacentOffsets = [(0-1,0),(0-1,0-1),(0,0-1),(1,0-1),(1,0),(1,1),(0,1),(0-1,1)]
+
+--Count number of neighbours of a cell
+--countNghb :: (Int, Int) -> Dict String (Int, Int) -> Int
+countNghb (x,y) gof = 
+    foldl (\(nx,ny) s -> if Dict.member (show (x+nx,y+ny)) gof then s+1 else s) 0 adjacentOffsets
+-- List of empty neighbours
+--emptyNghb :: (Int, Int) -> Dict String (Int, Int) -> [(Int,Int)]
+emptyNghb (x,y) gof = 
+    filter (not . (flip Dict.member gof) . show) $ map (\(nx,ny) -> (x+nx,y+ny)) adjacentOffsets
+
 
 --gofCell=   ((filled (rgb 255 102 0) .) .) . rect
 gofCell w h (x,y) =   filled (rgb 255 102 0) $ rect w h (x,y)
@@ -19,21 +32,33 @@ gofCell w h (x,y) =   filled (rgb 255 102 0) $ rect w h (x,y)
 noCell w h (x,y)  = filled (rgb 64 64 64) $ rect w h (x,y)
 
 gofAutomaton =
-    let fstep (x,y) cells = 
-        let out = (x/5, y/5):cells
-        in (out,out)
-    in init' ([(0,0)]) fstep
+    let { fstep pos initGof = 
+            let { survives cell gof = 
+                    let count = countNghb cell gof
+                    in count == 2 || count == 3
+                ; processCell cell gof' = 
+                    let firstPhase = if not $ survives cell initGof then Dict.remove (show cell) gof' else gof'
+                    in foldl (\empty nextGof -> if (countNghb empty initGof) == 3 
+                                                then Dict.insert (show empty) empty nextGof
+                                                else nextGof )
+                             firstPhase
+                             $ emptyNghb cell gof'
+                ; out = foldl processCell initGof $ Dict.values initGof}
+            in (Dict.values out, out)
+        ; exploder = [(8,8), (7,9), (8,9), (9,9), (7,10), (9,10), (8,11)]
+        ; gof = Dict.fromList $ zip (map show exploder) exploder }
+    in init' gof fstep
 
 synthCtrl w h = placeholder w h blue "Sythesizer controls"
 golCtrl w h = placeholder w h (rgb 56 56 56) "GoL controls"
 presets w h = placeholder w h (rgb 56 56 56) "Presets"
-sequencer w h = let { steps = 16.0
+sequencer w h = let { steps = 16
                     ; cellSize = toFloat(w)/steps
-                    ; coordinates = map (\x -> cellSize*toFloat(x) - cellSize*0.5) [1..steps]
-                    ; grid = concatMap (\c -> zip (repeat c steps) coordinates) coordinates
+                    ; coordinate x = cellSize*toFloat(x) - cellSize*0.5
+                    ; grid = concatMap (\c -> zip (repeat c steps) $ map coordinate [1..steps]) $ map coordinate [1..steps]
                     ; inactiveCells = map (noCell (cellSize*0.8) (cellSize*0.8)) $ grid }
                 in
-                    lift (\activeCells -> collage w h $ inactiveCells ++ (map (gofCell (cellSize*0.8) (cellSize*0.8)) activeCells))
+                    lift (\activeCells -> collage w h $ inactiveCells ++ (map (gofCell (cellSize*0.8) (cellSize*0.8)) $ map (\(x,y) -> (coordinate x, coordinate y)) activeCells))
                     $ run gofAutomaton $ sampleOn clicks position
 --                    $ run gofAutomaton $ (every 1)
 
