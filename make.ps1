e8a4edc493ab47b95ed95a7b62a92dabf6ca0d00
Rafal Lukaszewski <solarplexus6@gmail.com>
2013-01-23 00:08:16 +0100

    build system enhancement

--- make.ps1
+++ make.ps1
@@ -3,7 +3,10 @@ $elmFolder = "elm"
 $main = "main"
 $appMainPath = ".\app\public\"
 
+echo "Build started"
 Push-Location $elmFolder
 &elm --make ($main+".elm") -r $elmRuntime -i ..\synth\synth.js -i helper.js -i init.js
 Pop-Location
 Copy-Item (".\$elmFolder\$main.html") $appMainPath
+
+echo "Build finished"
